dc8b372d89923364c9d800708053ca62765b29bd
Chlorophyllkid <chlorophyllkid@gmail.com>
2018-04-07 19:05:40 +0200

    add styleguide-template package

---
+++ packages/styleguide-template/README.md
@@ -0,0 +1,6 @@
+
+# styleguide-template
+
+This project was bootstrapped with [Create Styleguide](https://github.com/chlorophyllkid/create-styleguide).
+
+It contains the predefined structure of the styleguide.
---
+++ packages/styleguide-template/init.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+const chalk = require('chalk');
+
+console.log(chalk.green('// ToDo: copy template to project folder :D'));
