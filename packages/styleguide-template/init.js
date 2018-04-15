331b7b96bff376a8d65bfabcbfb9de4e861b1801
Chlorophyllkid <chlorophyllkid@gmail.com>
2018-04-15 16:33:03 +0200

    add general init logic to styleguide-template

--- packages/styleguide-template/init.js
+++ packages/styleguide-template/init.js
@@ -6,6 +6,107 @@ process.on('unhandledRejection', err => {
   throw err;
 });
 
+const os = require('os');
+const fs = require('fs-extra');
+const path = require('path');
 const chalk = require('chalk');
+const spawn = require('cross-spawn');
 
-console.log(chalk.green('// ToDo: copy template to project folder :D'));
+
+module.exports = function(appPath, appName, originalDirectory) {
+  const ownPackageName = require(path.join(__dirname, '..', 'package.json')).name;
+  const ownPath = path.join(appPath, 'node_modules', ownPackageName);
+  const appPackage = require(path.join(appPath, 'package.json'));
+
+
+  appPackage.dependencies = appPackage.dependencies || {};
+
+  appPackage.scripts = {
+    start: 'react-scripts start',
+    build: 'react-scripts build',
+    test: 'react-scripts test --env=jsdom',
+    eject: 'react-scripts eject',
+  };
+
+  fs.writeFileSync(
+    path.join(appPath, 'package.json'),
+    JSON.stringify(appPackage, null, 2) + os.EOL
+  );
+
+
+  const templatePath = path.join(ownPath, 'template');
+
+  if (fs.existsSync(templatePath)) {
+    fs.copySync(templatePath, appPath);
+  } else {
+    console.error(`Could not locate supplied template: ${chalk.green(templatePath)}`);
+    return;
+  }
+
+  try {
+    fs.moveSync(
+      path.join(appPath, 'gitignore'),
+      path.join(appPath, '.gitignore'),
+      []
+    );
+  } catch (err) {
+    if (err.code === 'EEXIST') {
+      const data = fs.readFileSync(path.join(appPath, 'gitignore'));
+      fs.appendFileSync(path.join(appPath, '.gitignore'), data);
+      fs.unlinkSync(path.join(appPath, 'gitignore'));
+    } else {
+      throw err;
+    }
+  }
+
+  const command = 'npm';
+  let args = ['install', '--save'].filter(e => e);
+
+  const templateDependenciesPath = path.join(
+    appPath,
+    '.template.dependencies.json'
+  );
+  if (fs.existsSync(templateDependenciesPath)) {
+    const templateDependencies = require(templateDependenciesPath).dependencies;
+
+    args = args.concat(
+      Object.keys(templateDependencies).map(key => {
+        return `${key}@${templateDependencies[key]}`;
+      })
+    );
+    fs.unlinkSync(templateDependenciesPath);
+  }
+
+  const proc = spawn.sync(command, args, { stdio: 'inherit' });
+  if (proc.status !== 0) {
+    console.error(`\`${command} ${args.join(' ')}\` failed`);
+    return;
+  }
+
+  let cdpath;
+  if (originalDirectory && path.join(originalDirectory, appName) === appPath) {
+    cdpath = appName;
+  } else {
+    cdpath = appPath;
+  }
+
+  console.log();
+  console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log('Inside that directory, you can run several commands:');
+  console.log();
+  console.log(chalk.cyan('  npm start'));
+  console.log('    Starts the development server.');
+  console.log();
+  console.log(chalk.cyan('  npm run build'));
+  console.log('    Bundles the app into static files for production.');
+  console.log();
+  console.log(chalk.cyan('  npm test'));
+  console.log('    Starts the test runner.');
+  console.log();
+  console.log('We suggest that you begin by typing:');
+  console.log();
+  console.log(chalk.cyan('  cd'), cdpath);
+  console.log(`  ${chalk.cyan('npm start')}`);
+  console.log();
+  console.log('Happy hacking!');
+}
